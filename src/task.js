04423d35cf2f29fb0f70faa27a8e3104f0ec2b0c
Elkhan <elkhan.safarov10@yandex.ru>
2019-05-13 00:04:23 +0500

    Move Items between Columns with react-beautiful-dnd using onDragEnd

--- src/task.js
+++ src/task.js
@@ -10,6 +10,7 @@ const Container = styled.div`
 	background-color: ${props => (props.isDragging ? 'lightgreen' : 'white')};
 `;
 
+
 export default class Task extends React.Component {
 	render() {
 		return (
